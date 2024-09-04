7a4886180cbc9cbaee6dce07078e77f30b6922d2
Yonatan Israeli <yonirisraeli@gmail.com>
2024-09-04 03:52:06 -0400

    Update Windows Maintenance Tool.bat

--- Windows Maintenance Tool.bat
+++ Windows Maintenance Tool.bat
@@ -2,7 +2,7 @@
 setlocal
 title Windows Maintenance Tool
 echo Program Name: Windows Maintenance Tool
-echo Version: 2.0.0
+echo Version: 2.0.1
 echo Developer: @YonatanReuvenIsraeli
 echo Website: https://www.yonatanreuvenisraeli.dev
 echo License: GNU General Public License v3.0
@@ -23,9 +23,9 @@ echo [2] Reset OpenSSH client keys for user %USERNAME%.
 echo [3] Close
 set Start=
 set /p Start="What do you want to do? (1-3) "
-if "%Start%"=="1" goto "1"
-if "%Start%"=="2" goto "2"
-if "%Start%"=="3" goto "Close"
+if /i "%Start%"=="1" goto "1"
+if /i "%Start%"=="2" goto "2"
+if /i "%Start%"=="3" goto "Close"
 echo Invalid syntax!
 goto "Start"
 
